7a2149bdf3d97bedd398623509097e2a33928fc7
asaltycarebear <garbear.stockwell@gmail.com>
2025-09-08 11:54:06 -0600

    excercise 1&2 Done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        //Changed to float so decimals could be used. BYUI grades are in floats. Need to add message for not putting in a number later. 
+        float grade = float.Parse(Console.ReadLine());
+
+        string letter;
+
+
+        //Note to Self:
+        //Start High if using Greater than, Start Low if using Less than in the chain.
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        //This section is for stretch
+        //lastDigit was SO good to learn
+        if (letter != "F")
+        {
+            float lastDigit = grade % 10;
+
+            if (lastDigit >= 7 && letter != "A")  //The Cake is a lie, no A+ for you!
+            {
+                letter += "+";
+            }
+            else if (lastDigit < 3)
+            {
+                letter += "-";
+            }
+        }
+        //end strech
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Great jorb, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Keep working hard, you got this next time around!");
+        }
     }
 }
